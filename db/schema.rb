fcd5156aa25d577865d34da07d0aa0b5e180f219
marcianomatias <marcianomatias1@hotmail.com>
2023-03-27 18:37:51 -0300

    :sparkles: feat: criação da transaction!

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_193939) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_205624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.integer "balance_cents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transfers", force: :cascade do |t|
+    t.integer "balance_cents"
+    t.bigint "user_id", null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_transfers_on_company_id"
+    t.index ["user_id"], name: "index_transfers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +38,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_193939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "gender"
+    t.integer "balance_cents"
   end
 
+  add_foreign_key "transfers", "companies"
+  add_foreign_key "transfers", "users"
 end
